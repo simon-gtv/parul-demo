46bd30e3d13e91353f7ee2d577376fd2dccc0abd
parul6 <dhingraparul6@gmail.com>
2016-07-28 00:55:00 +0800

    Add files via upload

--- src/main/java/com/paurl/service/ServiceLayer.java
+++ src/main/java/com/paurl/service/ServiceLayer.java
@@ -1,4 +1,4 @@
-package com.paurl.service;
+/*package com.paurl.Service;
 
 import static java.lang.Integer.parseInt;
 import static java.util.stream.Collectors.toList;
@@ -20,6 +20,43 @@ public class ServiceLayer {
    public void doSomething(String parm) {
 
       System.out.println("value is: " + parm);
+   }
+
+}*/
+package com.paurl.Service;
+
+
+import org.json.JSONArray;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonAnyFormatVisitor;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonArrayFormatVisitor;
+
+@Service
+public class ServiceLayer {
+
+   // Business logic
+   public String doSomething(String parm) {
+	   JSONArray number = new JSONArray();
+	   for (int i =1 ; i<=Integer.parseInt(parm); i++){
+		   if(i%3 == 0 && i%5 == 0 ){
+			   number.put("fizz buzz");
+		   }
+		   else if(i % 3 == 0){
+			   number.put("fizz");
+		   }
+		   else if(i%5 == 0){
+			   number.put("buzz");
+		   }
+		    else{
+		   number.put(i);
+		    }
+		   
+	   }
+	   
+
+      System.out.println("value is: " + parm);
+      return number.toString();
    }
 
 }
